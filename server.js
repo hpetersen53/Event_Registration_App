3413eb80debb2017352831d8c782cfdab6e150c5
H. Petersen <hannpetersen53@gmail.com>
2024-12-13 22:55:13 -0500

    b

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
+
+
 const registrationsRouter = require("./api/registrations")
 const registerRouter = require("./api/register")
 
